c1b7de63fed9e72af0306f65ae5565ce6f862ed0
unknown <hienhahp1392005@gmail.com>
2025-08-28 23:41:39 +0700

    Initial commit for ASP.NET Core MVC project

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController:Controller
+    {
+        public IActionResult index()
+        {
+            ViewData["Title"] = "list";
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            ViewData["Title"] = $"detail - ID: {id}";
+            ViewData["ProductId"] = id;
+            return View();
+        }
+    }
+}
